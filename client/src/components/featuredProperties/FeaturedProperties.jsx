cb3e5fa3d752d728d1272c96d2e3db7f2a2d4458
OlaniyiHope <olaniyihoppee@gmail.com>
2022-10-20 07:55:42 +0100

    all

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,46 +1,43 @@
 import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
-import { Link } from "react-router-dom"
-import {
-  faLocationDot,
-} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const FeaturedProperties = ({ item }) => {
   const { data, loading, error } = useFetch("/hotels?featured=true");
-  const handleSearch = () => {
-
-  };
+  const handleSearch = () => {};
   return (
     <>
-      
-    <div className="fp"  >
-    
-      {loading ? (
-        "Loading"
-      ) : (
-        <>
-         
-          {data.map((item) => (
-            <div className="fpItem" key={item._id} >
-
-<Link to={`/hotels/${item._id}`}>
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
-              </Link>
-              <div className="fpTitle">
-              <span className="fpName">{item.name}</span><br></br>
-              <span className="fpCity"><bold><FontAwesomeIcon icon={faLocationDot} />{item.city}</bold> </span><br></br>
-              <span className="fpPrice"><bold>Price: N{item.cheapestPrice}</bold></span><br></br>
+      <div className="fp">
+        {loading ? (
+          "Loading"
+        ) : (
+          <>
+            {data?.map((item) => (
+              <div className="fpItem" key={item._id}>
+                <Link to={`/hotels/${item._id}`}>
+                  <img src={item.photos[0]} alt="" className="fpImg" />
+                </Link>
+                <div className="fpTitle">
+                  <span className="fpName">{item.name}</span>
+                  <br></br>
+                  <span className="fpCity">
+                    <bold>
+                      <FontAwesomeIcon icon={faLocationDot} />
+                      {item.city}
+                    </bold>{" "}
+                  </span>
+                  <br></br>
+                  <span className="fpPrice">
+                    <bold>Price: N{item.cheapestPrice}</bold>
+                  </span>
+                  <br></br>
+                </div>
               </div>
-            </div>
-          ))}
-        </>
-      )}
-      
-    </div>
+            ))}
+          </>
+        )}
+      </div>
     </>
   );
 };
