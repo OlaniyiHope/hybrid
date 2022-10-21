e77732e6bf55059b97ec0d80c260e86cdf11e5c1
OlaniyiHope <olaniyihoppee@gmail.com>
2022-10-21 03:21:31 +0100

    all

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -3,7 +3,7 @@ import "./featuredProperties.css";
 import { Link } from "react-router-dom";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const FeaturedProperties = ({ data }) => {
+const FeaturedProperties = ({ list }) => {
   const { data, loading, error } = useFetch("/hotels?featured=true");
   const handleSearch = () => {};
   return (
@@ -13,23 +13,23 @@ const FeaturedProperties = ({ data }) => {
           "Loading"
         ) : (
           <>
-            {data?.map((data) => (
-              <div className="fpItem" key={data?._id}>
-                <Link to={`/hotels/${data?._id}`}>
-                  <img src={data.photos[0]} alt="" className="fpImg" />
+            {data?.map((list) => (
+              <div className="fpItem" key={list?._id}>
+                <Link to={`/hotels/${list?._id}`}>
+                  <img src={list.photos[0]} alt="" className="fpImg" />
                 </Link>
                 <div className="fpTitle">
-                  <span className="fpName">{data?.name}</span>
+                  <span className="fpName">{list?.name}</span>
                   <br></br>
                   <span className="fpCity">
                     <bold>
                       <FontAwesomeIcon icon={faLocationDot} />
-                      {data?.city}
+                      {list?.city}
                     </bold>{" "}
                   </span>
                   <br></br>
                   <span className="fpPrice">
-                    <bold>Price: N{data?.cheapestPrice}</bold>
+                    <bold>Price: N{list?.cheapestPrice}</bold>
                   </span>
                   <br></br>
                 </div>
