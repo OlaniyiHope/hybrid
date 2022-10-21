064eb00c48e4805beda2c3ce382f40b38880ad50
OlaniyiHope <olaniyihoppee@gmail.com>
2022-10-21 08:46:03 +0100

    all

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -5,6 +5,7 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch("/hotels?featured=true");
+
   const handleSearch = () => {};
 
   return (
@@ -14,28 +15,29 @@ const FeaturedProperties = () => {
           "Loading"
         ) : (
           <>
-            {data.map((data) => (
-              <div className="fpItem" key={data?._id}>
-                <Link to={`/hotels/${data?._id}`}>
-                  <img src={data?.photos[0]} alt="" className="fpImg" />
-                </Link>
-                <div className="fpTitle">
-                  <span className="fpName">{data?.name}</span>
-                  <br></br>
-                  <span className="fpCity">
-                    <bold>
-                      <FontAwesomeIcon icon={faLocationDot} />
-                      {data?.city}
-                    </bold>{" "}
-                  </span>
-                  <br></br>
-                  <span className="fpPrice">
-                    <bold>Price: N{data?.cheapestPrice}</bold>
-                  </span>
-                  <br></br>
+            {data &&
+              data.map((data) => (
+                <div className="fpItem" key={data?._id}>
+                  <Link to={`/hotels/${data?._id}`}>
+                    <img src={data?.photos[0]} alt="" className="fpImg" />
+                  </Link>
+                  <div className="fpTitle">
+                    <span className="fpName">{data?.name}</span>
+                    <br></br>
+                    <span className="fpCity">
+                      <bold>
+                        <FontAwesomeIcon icon={faLocationDot} />
+                        {data?.city}
+                      </bold>{" "}
+                    </span>
+                    <br></br>
+                    <span className="fpPrice">
+                      <bold>Price: N{data?.cheapestPrice}</bold>
+                    </span>
+                    <br></br>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </>
         )}
       </div>
