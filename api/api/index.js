8cfd0c16bb02b24637429235b3b98a75596b0837
OlaniyiHope <olaniyihoppee@gmail.com>
2022-10-27 22:26:56 +0100

    take back last commit

--- api/api/index.js
+++ api/api/index.js
@@ -11,7 +11,7 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 
-/*const connect = async () => {
+const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
     console.log("Connected to mongoDB.");
@@ -23,18 +23,13 @@ dotenv.config();
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB has disconnected!");
 });
-*/
 
-const connect = async () => {
-  app.use("hello world");
-};
 //middlewares
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
-console.log("api route connected");
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
